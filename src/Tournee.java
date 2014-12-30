4424aa57fe4b8dfde77456bf8b177cd9d50d3928
samuelN7 <samuel.guery@outlook.com>
2014-12-30 12:29:02 +0100

    Create Tournee.java

---
+++ src/Tournee.java
@@ -0,0 +1,25 @@
+package projet_jee;
+import java.util.Collection;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+
+@Entity
+public class Tournee {
+
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    int id;
+    String titre;
+    String description;
+    @ManyToOne
+    Artiste artiste;
+    @OneToMany
+    Collection<Evenements> evenements;
+    
+
+
+
+}
